cdb70e84e2bdf36aa2368cc629722d7bd83892e9
hokinfai <AlanHo@hejianhuideMacBook-Pro.local>
2016-07-09 21:18:53 +0100

    Adding Regular Expression Features into the Training and testing data.
    In order to improve the performance of the Machine Learning, regular
    expression is applied.

---
+++ RegularExpressionTrainingFeatures.java
@@ -0,0 +1,161 @@
+package MachineLearningDictionaryFeatures;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RegularExpressionTrainingFeatures {
+	List<String> list = new ArrayList<String>();
+
+	public RegularExpressionTrainingFeatures(List<String> input) {
+		this.list = input;
+	}
+
+	public void addFeatures() throws IOException {
+		for (int i = 0; i < list.size(); i++) {
+			String inputData = list.get(i);
+			String[] spilter = inputData.split("\t");
+			String namedEntity = spilter[3];
+			if (namedEntity.matches("^(Dr|Miss|Ms|Sir|Madam)$"))
+				list.set(i, inputData + "\tB-NamedEntity_Title");
+			else
+				list.set(i, inputData + "\tO");
+		}
+		for (int i = 0; i < list.size(); i++) {
+			String inputData = list.get(i);
+			String[] spilter = inputData.split("\t");
+			String originalForm = spilter[4];
+			String namedEntity = spilter[3];
+			String chunk = spilter[6];
+			if (originalForm.equals(namedEntity.trim()) && !chunk.equals("O") && spilter[5].matches("NNP\\w?")) {
+				if (namedEntity.matches("[A-Z][a-z][a-z]+") || namedEntity.matches("[A-Z][A-Z]+"))
+					list.set(i, inputData + "\t" + chunk.substring(0, 2) + "NamedEntity_ProperName");
+				else
+					list.set(i, inputData + "\tO");
+			} else
+				list.set(i, inputData + "\tO");
+		}
+		for (int i = 0; i < list.size(); i++) {
+			String inputData = list.get(i);
+			String[] spilter = inputData.split("\t");
+			String chunk = spilter[6];
+			String namedEntity = spilter[3];
+			if (namedEntity.matches("(16|17|18|19|20)\\d{2}(-\\d+)?") && !chunk.equals("O"))
+				list.set(i, inputData + "\t" + chunk.substring(0, 2) + "Time_Year");
+			else if (namedEntity.matches("(16|17|18|19|20)\\d{2}(-\\d+)?") && chunk.equals("O"))
+				list.set(i, inputData + "\t" + "B-Time_Year");
+			else
+				list.set(i, inputData + "\tO");
+		}
+		for (int i = 0; i < list.size(); i++) {
+			String inputData = list.get(i);
+			String[] spilter = inputData.split("\t");
+			String chunk = spilter[6];
+			String namedEntity = spilter[3];
+			if (namedEntity.matches("^(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\\w+?"))
+				list.set(i, inputData + "\t" + chunk.substring(0, 2) + "Time_Month");
+			else
+				list.set(i, inputData + "\tO");
+		}
+		for (int i = 0; i < list.size(); i++) {
+			String inputData = list.get(i);
+			String[] spilter = inputData.split("\t");
+			String namedEntity = spilter[3];
+			String chunk = spilter[6];
+			if (namedEntity.matches(
+					"(current(ly)?|recent(ly)?|soon(\\w*)|after|before|night|day|annual(ly)?|early|earlier|later|old|spring|autumn|summer|winter)"))
+				list.set(i, inputData + "\t" + chunk.substring(0, 2) + "Time_Phase");
+			// else if
+			// (namedEntity.matches("(year(s)?|month(s)?|day(s)?|week(s)?)"))
+			// list.set(i, inputData + "\tI-Time_Phase");
+			else
+				list.set(i, inputData + "\tO");
+		}
+
+		for (int i = 0; i < list.size(); i++) {
+			String inputData = list.get(i);
+			String[] spilter = inputData.split("\t");
+			String namedEntity = spilter[3];
+			String color = "^gray|blue|pink|red|white|black|yellow|gold|orange|dark|brown|purple|violet|silver|green|lime|jade|navy$";
+			if (namedEntity.matches(color))
+				list.set(i, inputData + "\t" + "B-Color");
+			else
+				list.set(i, inputData + "\tO");
+		}
+
+		for (int i = 0; i < list.size(); i++) {
+			String inputData = list.get(i);
+			String[] spilter = inputData.split("\t");
+			String namedEntity = spilter[3];
+			if (namedEntity.trim().matches("^cm|mm|meters$")) {
+				list.set(i - 1, list.get(i - 1).substring(0, list.get(i - 1).length() - 1) + "B-Object_size");
+				list.set(i, inputData + "\t" + "I-Object_size");
+			} else
+				list.set(i, inputData + "\tO");
+		}
+
+		for (int i = 0; i < list.size(); i++) {
+			String inputData = list.get(i);
+			String[] spilter = inputData.split("\t");
+			String namedEntity = spilter[3];
+			if (namedEntity.matches("\\d+-\\w+ed$"))
+				list.set(i, inputData + "\t" + "B-Size_Description");
+			else
+				list.set(i, inputData + "\tO");
+		}
+		for (int i = 0; i < list.size(); i++) {
+			String inputData = list.get(i);
+			String[] spilter = inputData.split("\t");
+			String namedEntity = spilter[3];
+			if (namedEntity.matches("([sS]outh|[nN]orth|[wW]est|[eE]ast)(ern)?"))
+				list.set(i, inputData + "\t" + "B-Cardinal_Direction");
+			else
+				list.set(i, inputData + "\tO");
+		}
+
+		toPrint();
+
+	}
+
+	public void toPrint() throws IOException {
+		StringBuilder output = new StringBuilder();
+		for (String ele : list) {
+			System.out.println(ele);
+			output.append(ele + "\n");
+		}
+		String printing = output.toString();
+		File file = new File(
+				"/Users/AlanHo/Documents/DissertationLibrary/NERsuite/bin/Training Data/Dictionary Features/TrainingData(dictionary).txt");
+		// creates the file
+		file.createNewFile();
+		// creates a FileWriter Object
+		FileWriter writer = new FileWriter(file);
+		// Writes the content to the file
+		writer.write(printing.toString());
+		writer.flush();
+		writer.close();
+	}
+
+	public static void main(String args[]) throws IOException {
+		String trainingDataPath = "/Users/AlanHo/Documents/DissertationLibrary/NERsuite/bin/Training Data/No Features/Training.txt";
+
+		BufferedReader br = new BufferedReader(new FileReader(trainingDataPath));
+		List<String> input = new ArrayList<String>();
+		try {
+			String line = br.readLine();
+			while (line != null) {
+				if (line != null)
+					input.add(line);
+				line = br.readLine();
+			}
+		} finally {
+			br.close();
+			new RegularExpressionTrainingFeatures(input).addFeatures();
+		}
+
+	}
+}
